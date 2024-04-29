6a26232ea33d59f44aa25c7d67151b945b9eaecb
uptu <uptu@uptu.dev>
2024-04-29 11:17:24 -0600

    Remove excessive comments in CLAP Args struct

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use clap::Parser;
 
 /* Argument parsing */
 
-/// Help text styling
 fn styles() -> Styles {
     Styles::styled()
         .header(AnsiColor::Yellow.on_default() | Effects::BOLD)
@@ -14,8 +13,7 @@ fn styles() -> Styles {
         .placeholder(AnsiColor::Green.on_default())
 }
 
-/// Builds a help template (because version number may change the length of the box needed)
-pub fn gen_help_template() -> String {
+fn gen_help_template() -> String {
     const VER_LEN: usize = env!("CARGO_PKG_VERSION").len() - 5;
     const BASE: &'static str = "\
 \x1b[0;90m┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
@@ -40,34 +38,31 @@ pub fn gen_help_template() -> String {
     out
 }
 
-/// The about and author strings for --help
 const ABOUT: &'static str = "\x1b[0;1;95;4mPi\x1b[0m is a CLI \x1b[94mproject initializer\x1b[0m which uses per-language build tools to create project directories populated with a full project template.";
 const AUTHOR: &'static str = "by \x1b[0;93muptu\x1b[0m <\x1b[33muptu@uptu.dev\x1b[0m>";
 
 #[derive(Parser, Debug)]
-#[command(                      // This preprocessor block deals mostly with the styling of --help
+#[command(
     version,                    // enables `-v` and `--version` to print the version
-    about = ABOUT,              // the project about text const goes here
-    author = AUTHOR,            // the above author text const goes here
-    styles = styles(),          // uses `styles()` fn to format most of the output
+    about = ABOUT,
+    author = AUTHOR,
+    styles = styles(),
     long_about = None,          // the long version of `-h`, `--help` has no special text
     next_line_help = true,      // option help info is given on a new line
     help_template = gen_help_template()
 )]
-/// The arguments to be pushed to the rest of the program
 struct Args {
     /// Name of the project
     #[arg(
-        name = "NAME",          // the text to put in the placeholder in the help text
-        short = 'n',            // the short version of the arg flag (i.e. `-n` here)
-        long = "name",          // the long version of the arg flag (i.e. `--name` here)
+        name = "NAME",
+        short = 'n',
+        long = "name",
         verbatim_doc_comment    // speeds up preprocessing of the doc comment above
     )]
     name: Option<String>,
-
     /// Target language of the project
     #[arg(
-        name = "LANGUAGE",      // see above for field documentation
+        name = "LANGUAGE",
         short = 'l', 
         long = "lang", 
         verbatim_doc_comment
@@ -75,6 +70,7 @@ struct Args {
     lang: Option<String>,
 }
 
+/* Entry point */
 
 fn main() {
     let args = Args::parse();
